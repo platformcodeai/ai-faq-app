fb73385ccdc1efd383404c2ab1530e161d2a20e7
Réberth Rsilva <rsilvareberth@gmail.com>
2025-02-15 15:13:45 -0300

    chore: initial commit

---
+++ src/test/java/ai/platformcode/diagram/DiagramApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.platformcode.diagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
